2c948bc00891ef279d59937dcc05054aba654993
Luxolo Gqadu <luwie@Luxolos-MacBook-Air.local>
2025-07-06 14:28:30 +0100

    form no, views no, and urls created

--- Recociliator_J/recociliator/yes_views.py
+++ Recociliator_J/recociliator/yes_views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .form import SplitYesForm
+from .form_yes import SplitYesForm
 
 # 🎯 Displays the form for the user to input all the data
 def split_yes_input(request):

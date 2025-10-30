e0ee647e76a0c695fc5a5bbef6169bc0226c0173
Luxolo Gqadu <luwieluxolo@gmail.com>
2025-10-30 16:07:12 +0000

    Set ALLOWED_HOSTS to ['*'] for Railway troubleshooting

--- Recociliator_J/Recociliator_J/settings.py
+++ Recociliator_J/Recociliator_J/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get(
-    'ALLOWED_HOSTS', 'localhost,127.0.0.1,testserver,*.railway.app').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
